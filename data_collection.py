5c71958a9486488da6f2a2199d77c62f11df18c2
jyothish2312 <42204656+jyothish2312@users.noreply.github.com>
2022-08-08 22:52:08 +0530

    Add files via upload
    v2

--- data_collection.py
+++ data_collection.py
@@ -5,6 +5,8 @@ import smbus
 import pyfirmata
 import time
 import csv
+import spidev               # To communicate with SPI devices
+from numpy import interp    # To scale values
 import RPi.GPIO as GPIO
 from datetime import datetime
 
@@ -16,21 +18,30 @@ curr_date_time = now.strftime("%d-%b-%Y %I:%M:%S %p")
 board = pyfirmata.Arduino('/dev/ttyACM0')
 print("Firmata Communication Successfully started")
 
+# Start SPI connection
+spi = spidev.SpiDev() # Created an object
+spi.open(0,0)
+print("ADC Communication Started")	
+
+
+button = board.digital[12]  #Arduino Pin
+red = board.digital[8]      #Arduino Pin
+green = board.digital[9]    #Arduino Pin
+blue = board.digital[10]    #Arduino Pin
+
+button.mode = pyfirmata.INPUT   # declaring button for digitalRead
+
 en = 22
-pwm = 15
-ina = 16
-inb = 18
-logpin = 13 
+pwm = 32
+ina = 13
+inb = 15
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(logpin, GPIO.IN)
 GPIO.setup(en, GPIO.OUT)
 GPIO.setup(pwm, GPIO.OUT)
 GPIO.setup(ina, GPIO.OUT)
 GPIO.setup(inb, GPIO.OUT)
 
-
-
-    #some MPU6050 Registers and their Address
+#some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
 CONFIG       = 0x1A
@@ -69,6 +80,12 @@ def read_raw_data(addr):
                 value = value - 65536
         return value
 
+# Read MCP3008 data
+def analogInput(channel):
+  spi.max_speed_hz = 1350000
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
 
 bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 time.sleep(1)
@@ -80,21 +97,17 @@ print ("IMU  initiated - Reading Data of Gyroscope and Accelerometer")
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-red = 8
-green = 9
-blue = 10
+# Uncomment the following if you are using arduino analog input instead of ADC Chip
+# forceA = board.analog[0]
+# forceB = board.analog[1]
+# forceC = board.analog[2]
+# forceD = board.analog[3]
 
-
-forceA = board.analog[0]
-forceB = board.analog[1]
-forceC = board.analog[2]
-forceD = board.analog[3]
-
-forceA.enable_reporting()
-forceB.enable_reporting()
-forceC.enable_reporting()
-forceD.enable_reporting()
-print("Force sensors activated")
+# forceA.enable_reporting()
+# forceB.enable_reporting()
+# forceC.enable_reporting()
+# forceD.enable_reporting()
+# print("Force sensors activated")
     
 storagepath = '/home/pi/Jyothish/data_'+ label + '.csv'
 print(storagepath)
@@ -122,22 +135,39 @@ with open(storagepath, 'a', encoding= 'UTF8') as f:
 
 
     j=0
+    n=0
     while True:
         # board.digital[blue].write(1)
         time.sleep(0.1)
-        #Read Accelerometer raw value
-        acc_x = read_raw_data(ACCEL_XOUT_H)
-        acc_y = read_raw_data(ACCEL_YOUT_H)
-        acc_z = read_raw_data(ACCEL_ZOUT_H)
-        #Read Gyroscope raw value
-        gyro_x = read_raw_data(GYRO_XOUT_H)
-        gyro_y = read_raw_data(GYRO_YOUT_H)
-        gyro_z = read_raw_data(GYRO_ZOUT_H)
-        #Read Force Sensor values
-        fA = forceA.read()
-        fB = forceB.read()
-        fC = forceC.read()
-        fD = forceD.read()
+        
+        try:
+            #Read Accelerometer raw value
+            acc_x = read_raw_data(ACCEL_XOUT_H)
+            acc_y = read_raw_data(ACCEL_YOUT_H)
+            acc_z = read_raw_data(ACCEL_ZOUT_H)
+            #Read Gyroscope raw value
+            gyro_x = read_raw_data(GYRO_XOUT_H)
+            gyro_y = read_raw_data(GYRO_YOUT_H)
+            gyro_z = read_raw_data(GYRO_ZOUT_H)
+
+            n=n+1
+            
+        except:
+            n=n+1
+            continue
+        #Read Force Sensor values  : If using Arduino instead of ADC
+        # fA = forceA.read()
+        # fB = forceB.read()
+        # fC = forceC.read()
+        # fD = forceD.read()
+
+        # Read Force Sensor values : ADC type.
+        fA = analogInput(0) # Reading from CH0
+        fB = analogInput(1) # Reading from CH0
+        fC = analogInput(2) # Reading from CH0
+        fD = analogInput(3) # Reading from CH0
+        
+
         #Full scale range +/- 250 degree/C as per sensitivity scale factor
         Ax = acc_x/16384.0
         Ay = acc_y/16384.0
@@ -146,9 +176,9 @@ with open(storagepath, 'a', encoding= 'UTF8') as f:
         Gy = gyro_y/131.0
         Gz = gyro_z/131.0
 
-        row = [curr_date_time, round(Ax,2), round(Ay,2), round(Az,2), round(Gx,2), round(Gy,2), round(Gz,2), fA, fB, fC, fD, label]
+        row = [n, curr_date_time, round(Ax,2), round(Ay,2), round(Az,2), round(Gx,2), round(Gy,2), round(Gz,2), fA, fB, fC, fD, label]
         
-        if (GPIO.input(logpin)) == 1:
+        if (button.read()) == 1:
             writer.writerow(row)
             print (row)
             # board.digital[blue].write(0)
@@ -161,6 +191,7 @@ with open(storagepath, 'a', encoding= 'UTF8') as f:
             # board.digital[green].write(0)
             if j == 0:
                 nextset = ['-------------------------------------']
+                n=0
                 writer.writerow(nextset)
                 print(nextset)
                 board.digital[green].write(0)
